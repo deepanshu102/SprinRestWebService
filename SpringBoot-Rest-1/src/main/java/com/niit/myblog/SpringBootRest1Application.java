b40c24f7e8cb92b915bb3f552782fc45fc6f0e91
root <root@deep-Lenovo-G50-45>
2020-04-03 17:51:38 +0530

    "done"

--- SpringBoot-Rest-1/src/main/java/com/niit/myblog/SpringBootRest1Application.java
+++ SpringBoot-Rest-1/src/main/java/com/niit/myblog/SpringBootRest1Application.java
@@ -1,13 +1,37 @@
 package com.niit.myblog;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import com.niit.myblog.model.Quote;
 
 @SpringBootApplication
 public class SpringBootRest1Application {
 
+//	private static final Logger log=LoggerFactory.getLogger(SpringBootApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRest1Application.class, args);
 	}
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
+	@Bean
+	public CommandLineRunner run( RestTemplate restTemplate) throws Exception{
+	return args->{
+		Quote quote=restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+//		log.info(quote.toString());
+		System.out.println(quote.toString());
+		
+		
+	};	}
 
 }
